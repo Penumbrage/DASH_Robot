d6652a8a4fb23d23e612e36da64758c2757d7049
William Wang <wiwa3412@colorado.edu>
2022-03-16 21:01:55 -0600

    The following build for the bluetooth is not functioning

--- bluetooth_testing/bluetooth_testing.ino
+++ bluetooth_testing/bluetooth_testing.ino
@@ -1,3 +1,41 @@
+//#include <SoftwareSerial.h>
+//SoftwareSerial HM10(0,1); // RX = 2, TX = 3
+//char appData;  
+//String inData = "";
+//void setup()
+//{
+//  Serial.begin(9600);
+//  Serial.println("HM10 serial started at 9600");
+//  HM10.begin(9600); // set HM10 serial at 9600 baud rate
+//}
+//
+//void loop()
+//{
+//  HM10.listen();  // listen the HM10 port
+//  while (HM10.available() > 0) {   // if HM10 sends something then read
+//    appData = HM10.read();
+//    inData = String(appData);  // save the data in string format
+//    Serial.write(appData);
+//  }
+//
+// 
+//  if (Serial.available()) {           // Read user input if available.
+//    delay(10);
+//    HM10.write(Serial.read());
+//  }
+//  if ( inData == "F") {
+//    Serial.println("LED OFF");
+//    delay(500);
+//  }
+//  if ( inData == "N") {
+//    Serial.println("LED ON");
+//    delay(500);
+//  }
+//}
+
+
+
+
 /*
 This code serves as a test for the communication between the bluetooth module
 and the Arduino
@@ -32,7 +70,9 @@ void setup()
 
 void loop()
 {
-  
+   get_msg();
+   set_current_state();
+   motor_state_machine();
 }
 
 // This function gets a message from the bluetooth module when available
@@ -40,51 +80,32 @@ void get_msg(){
     HM10.listen();  // listen the HM10 port
     while (HM10.available() > 0) {   // if HM10 sends something then read
         bt_msg = HM10.read();
-        cmd = String(appData);  // save the data in string format
-        Serial.write(appData);
-
+        cmd = String(bt_msg);  // save the data in string format
+        Serial.print("The following command has been received: "); Serial.println(cmd);
+    }
     if (Serial.available()) {           // Read user input if available.
         delay(10);
-        HM10.write(Serial.read());
-    }
-    if ( inData == "F") {
-        Serial.println("LED OFF");
-        delay(500);
-    }
-    if ( inData == "N") {
-        Serial.println("LED ON");
-        delay(500);
+        HM10.write("HEY");
     }
 }
 
 // This function gets the current state information from the bluetooth module
-void get_current_state(){
-   // TODO: FInd a method to allow for different motor speeds to be inputed from the serial line
+void set_current_state(){
    // Obtains a message from the serial line and updates the state case
-   if (Serial.available()){
-      // get message from serial line
-      int msg = Serial.parseInt();
-
-      // update the state based on the message
-      if (msg == 1){
-         currentState = STOP;
-      }
-      else if (msg == 2){
-         currentState = FORWARD;
-      }
-      else if (msg == 3){
-         currentState = BACKWARD;
-      }
-      else if (msg == 4){
-         currentState = LEFT;
-      }
-      else if (msg == 5){
-         currentState = RIGHT;
-      }
+   if (cmd == "1"){
+      currentState = STOP;
    }
-   else{
-      // TODO: figure out if this code is necessary
-      currentState = currentState;
+   else if (cmd == "2"){
+      currentState = FORWARD;
+   }
+   else if (cmd == "3"){
+      currentState = BACKWARD;
+   }
+   else if (cmd == "4"){
+      currentState = LEFT;
+   }
+   else if (cmd == "5"){
+      currentState = RIGHT;
    }
 }
 
@@ -97,29 +118,29 @@ void motor_state_machine(){
    {
       case STOP:
          // both motors off and the robot is not moving
-         brake(motorLeft, motorRight);
+         HM10.write("The current state is: "); HM10.write(STOP);
          break;
 
       case FORWARD:
          // moves the robot forward
-         forward(motorLeft, motorRight, DEFAULT_SPD);
+         HM10.write("The current state is: "); HM10.write(STOP);
          break;
 
       case BACKWARD:
          // moves the robot backward
-         back(motorLeft, motorRight, DEFAULT_SPD);
+         HM10.write("The current state is: "); HM10.write(STOP);
          break;
 
       case LEFT:
          // turns the robot left
          // TODO: figure out why power is decreased for this condition
-         left(motorLeft, motorRight, DEFAULT_SPD);
+         HM10.write("The current state is: "); HM10.write(STOP);
          break;
          
       case RIGHT:
          // turns the robot right
          // TODO: figure out why power is decreased for ths condition
-         right(motorLeft, motorRight, DEFAULT_SPD);
+         HM10.write("The current state is: "); HM10.write(STOP);
          break;
    }
 }
