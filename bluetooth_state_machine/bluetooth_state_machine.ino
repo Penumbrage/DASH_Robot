27199d7cc97914434530fd51f59851404cc0b412
William Wang <wiwa3412@colorado.edu>
2022-03-17 17:39:52 -0600

    implemented the bluetooth control with the motor driver state machine

---
+++ bluetooth_state_machine/bluetooth_state_machine.ino
@@ -0,0 +1,145 @@
+/******************************************************************************
+TestRun.ino
+TB6612FNG H-Bridge Motor Driver Example code
+Michelle @ SparkFun Electronics
+8/20/16
+https://github.com/sparkfun/SparkFun_TB6612FNG_Arduino_Library
+
+Uses 2 motors to show examples of the functions in the library.  This causes
+a robot to do a little 'jig'.  Each movement has an equal and opposite movement
+so assuming your motors are balanced the bot should end up at the same place it
+started.
+
+Resources:
+TB6612 SparkFun Library
+
+Development environment specifics:
+Developed on Arduino 1.6.4
+Developed with ROB-9457
+******************************************************************************/
+
+// Include the different libraries in the code
+#include <SoftwareSerial.h>
+#include <SparkFun_TB6612.h>
+
+// Create a bluetooth object and variables to store data read from bluetooth
+SoftwareSerial HM10(0, 1); // RX = 0, TX = 1
+char bt_msg;               // stores message from bluetooth module
+String cmd = "";           // command to send to motor driver
+
+// Pins for all inputs, keep in mind the PWM defines must be on PWM pins
+// the default pins listed are the ones used on the Redbot (ROB-12097) with
+// the exception of STBY which the Redbot controls with a physical switch
+#define AIN1 2
+#define BIN1 6
+#define AIN2 4
+#define BIN2 7
+#define PWMA 3
+#define PWMB 5
+#define STBY 9
+
+// Define the default speed of the motors by using the PWM to get to 3.3 V
+// TODO: Tune the motor speed to mimic our original testing case
+#define DEFAULT_SPD 200
+
+// these constants are used to allow you to make your motor configuration 
+// line up with function names like forward.  Value can be 1 or -1
+const int offsetA = 1;
+const int offsetB = 1;
+
+// Initializing motors.  The library will allow you to initialize as many
+// motors as you have memory for.  If you are using functions like forward
+// that take 2 motors as arguements you can either write new functions or
+// call the function more than once.
+Motor motorLeft = Motor(AIN1, AIN2, PWMA, offsetA, STBY);
+Motor motorRight = Motor(BIN1, BIN2, PWMB, offsetB, STBY);
+
+// Initialize an enumeration to describe the different states for the motors
+typedef enum{
+   FORWARD,
+   BACKWARD,
+   LEFT,
+   RIGHT,
+   STOP
+} motorState;
+
+// Initialize the state of the motor (STOP)
+motorState currentState = STOP;
+
+void setup()
+{
+   HM10.begin(9600); // set HM10 serial at 9600 baud rate
+   HM10.println("Bluetooth connection established.");
+}
+
+void loop()
+{
+   get_msg();
+   get_current_state();
+   motor_state_machine();
+}
+
+// This function gets a message from the bluetooth module when available
+void get_msg(){
+    HM10.listen();  // listen the HM10 port
+    while (HM10.available() > 0) {   // if HM10 sends something then read
+        bt_msg = HM10.read();
+        cmd = String(bt_msg);  // save the data in string format
+        HM10.print("The following command has been received: "); HM10.println(cmd); HM10.println("");
+    }
+}
+
+// This function gets the current state information from the bluetooth module
+void set_current_state(){
+   // Obtains a message from the serial line and updates the state case
+   if (cmd == "0"){
+      currentState = STOP;
+   }
+   else if (cmd == "1"){
+      currentState = FORWARD;
+   }
+   else if (cmd == "2"){
+      currentState = BACKWARD;
+   }
+   else if (cmd == "3"){
+      currentState = LEFT;
+   }
+   else if (cmd == "4"){
+      currentState = RIGHT;
+   }
+}
+
+void motor_state_machine(){
+   // TODO: Implement varying speeds into the state machines based on the joystick
+   // Develop a simple state machine for the robot that controls the motors
+   // with the following states: FORWARD, BACKWARD, LEFT, RIGHT, STOP
+   switch(currentState)
+   {
+      case STOP:
+         // both motors off and the robot is not moving
+         brake(motorLeft, motorRight);
+         break;
+
+      case FORWARD:
+         // moves the robot forward
+         forward(motorLeft, motorRight, DEFAULT_SPD);
+         break;
+
+      case BACKWARD:
+         // moves the robot backward
+         back(motorLeft, motorRight, DEFAULT_SPD);
+         break;
+
+      case LEFT:
+         // turns the robot left
+         // TODO: figure out why power is decreased for this condition
+         left(motorLeft, motorRight, DEFAULT_SPD);
+         break;
+         
+      case RIGHT:
+         // turns the robot right
+         // TODO: figure out why power is decreased for ths condition
+         right(motorLeft, motorRight, DEFAULT_SPD);
+         break;
+   }
+}
