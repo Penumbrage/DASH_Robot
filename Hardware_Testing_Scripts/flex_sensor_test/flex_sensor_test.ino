04fbc1dfac3d4e52de21a8d6b5263d4e911d2280
Penumbrage <wwangVer2.0@gmail.com>
2022-03-23 01:17:52 -0600

    added functioning flex sensor code

--- Hardware_Testing_Scripts/flex_sensor_test/flex_sensor_test.ino
+++ Hardware_Testing_Scripts/flex_sensor_test/flex_sensor_test.ino
@@ -5,26 +5,26 @@ Example sketch for SparkFun's flex sensors
 Jim Lindblom @ SparkFun Electronics
 April 28, 2016
 
-Create a voltage divider circuit combining a flex sensor with a 47k resistor.
+Create a voltage divider circuit combining a flex sensor with a 10k resistor.
 - The resistor should connect from A0 to GND.
-- The flex sensor should connect from A0 to 3.3V
+- The flex sensor should connect from A0 to 5V
 As the resistance of the flex sensor increases (meaning it's being bent), the
 voltage at A0 should decrease.
 
 Development environment specifics:
 Arduino 1.6.7
 ******************************************************************************/
-const int FLEX_PIN = A0; // Pin connected to voltage divider output
+#define FLEX_PIN 18 // Pin connected to voltage divider output
 
 // Measure the voltage at 5V and the actual resistance of your
-// 47k resistor, and enter them below:
-const float VCC = 4.98; // Measured voltage of Ardunio 5V line
-const float R_DIV = 47500.0; // Measured resistance of 3.3k resistor
+// 10k resistor, and enter them below:
+const float VCC = 5.0; // Measured voltage of Ardunio 5V line
+const float R_DIV = 9730; // Measured resistance of 10k resistor
 
 // Upload the code, then try to adjust these values to more
 // accurately calculate bend degree.
-const float STRAIGHT_RESISTANCE = 37300.0; // resistance when straight
-const float BEND_RESISTANCE = 90000.0; // resistance at 90 deg
+const float STRAIGHT_RESISTANCE = 24500.0; // resistance when straight
+const float BEND_RESISTANCE = 65000.0; // resistance at 90 deg
 
 void setup() 
 {
