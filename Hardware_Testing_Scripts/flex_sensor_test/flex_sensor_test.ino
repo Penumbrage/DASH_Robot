9536d9cf23031245518a5d3a39a4bac2d8afa577
William Wang <wiwa3412@colorado.edu>
2022-04-06 15:55:45 -0600

    changed the flex sensor test to work with bluetooth

--- Hardware_Testing_Scripts/flex_sensor_test/flex_sensor_test.ino
+++ Hardware_Testing_Scripts/flex_sensor_test/flex_sensor_test.ino
@@ -14,21 +14,25 @@ voltage at A0 should decrease.
 Development environment specifics:
 Arduino 1.6.7
 ******************************************************************************/
-#define FLEX_PIN 18 // Pin connected to voltage divider output
+#include <SoftwareSerial.h>
+SoftwareSerial HM10(14, 15); // RX = 14, TX = 15
+#define FLEX_PIN A0 // Pin connected to voltage divider output
 
 // Measure the voltage at 5V and the actual resistance of your
 // 10k resistor, and enter them below:
-const float VCC = 5.0; // Measured voltage of Ardunio 5V line
+const float VCC = 5.15; // Measured voltage of Ardunio 5V line
 const float R_DIV = 10000; // Measured resistance of 10k resistor
 
 // Upload the code, then try to adjust these values to more
 // accurately calculate bend degree.
-const float STRAIGHT_RESISTANCE = 51000; // resistance when straight
-const float BEND_RESISTANCE = 120000; // resistance at 90 deg
+const float STRAIGHT_RESISTANCE = 80000; // resistance when straight
+const float BEND_RESISTANCE = 300000; // resistance at 90 deg
 
 void setup() 
 {
-  Serial.begin(9600);
+//  Serial.begin(9600);
+  HM10.begin(9600); // set HM10 serial at 9600 baud rate
+  HM10.println("Bluetooth connection established.");
   pinMode(FLEX_PIN, INPUT);
 }
 
@@ -38,14 +42,14 @@ void loop()
   int flexADC = analogRead(FLEX_PIN);
   float flexV = flexADC * VCC / 1023.0;
   float flexR = R_DIV * (VCC / flexV - 1.0);
-  Serial.println("Resistance: " + String(flexR) + " ohms");
+  HM10.println("Resistance: " + String(flexR) + " ohms");
 
   // Use the calculated resistance to estimate the sensor's
   // bend angle:
   float angle = map(flexR, STRAIGHT_RESISTANCE, BEND_RESISTANCE,
                    0, 90.0);
-  Serial.println("Bend: " + String(angle) + " degrees");
-  Serial.println();
+  HM10.println("Bend: " + String(angle) + " degrees");
+  HM10.println();
 
-  delay(500);
+  delay(1000);
 }
